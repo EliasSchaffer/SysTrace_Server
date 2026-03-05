31bb76ef54957a4a6fc9ec4bbf402a12fefda1e0
EliasSchaffer <eliasschaffer13@gmail.com>
2026-03-05 14:59:28 +0100

    Add HTTP server with basic handlers for root and metrics

--- main.go
+++ main.go
@@ -1,5 +1,27 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"net/http"
+)
 
+func handler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Hello SysTrace Dashboard")
+}
+
+func main() {
+	http.HandleFunc("/", handler)
+
+	fmt.Println("Server läuft auf :8080")
+
+	http.HandleFunc("/metrics", metricsHandler)
+
+	http.ListenAndServe(":8080", nil)
+
+}
+
+func metricsHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("Metrics received")
+
+	w.WriteHeader(http.StatusOK)
 }
